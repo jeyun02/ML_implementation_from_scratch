af99cf8504acb22337325aacb665cf7c899f1b53
jeyun02 <101978016+jeyun02@users.noreply.github.com>
2024-12-09 16:37:56 +0900

    commit_2
    주석 추가

--- 1. KNN/test.py
+++ 1. KNN/test.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
+from KNN import KNN
 
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
@@ -14,48 +15,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
-cmap = ListedColormap(['#FF0000','#00FF00','#0000FF']) # red green blue 의 color hex code 임.
+cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
 
 plt.figure()
 plt.scatter(X[:,2],
             X[:,3], 
             c=y, 
             cmap=cmap, 
-            edgecolors='k', 
+            edgecolor='k', 
             s=20)
 plt.show()
 """
 
-from KNN import KNN
-
-clf = KNN(k=5) # class instance 생성
-clf.fit(X_train, y_train)
-predictions = clf.predict(X_test)   
-
-print(predictions)
-
-
-
-
-"""import numpy as np
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from KNN import KNN
-
-cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
-
-iris = datasets.load_iris()
-X, y = iris.data, iris.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
-
-plt.figure()
-plt.scatter(X[:,2],X[:,3], c=y, cmap=cmap, edgecolor='k', s=20)
-plt.show()
-
-
+# knn 클래스 인스턴스 생성
 clf = KNN(k=5)
 # 데이터 클래스로 전달.
 clf.fit(X_train, y_train)
