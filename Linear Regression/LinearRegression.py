7184d1959a1f6a2b92b9435c9712a36260e5ee71
jeyun02 <101978016+jeyun02@users.noreply.github.com>
2024-12-09 18:34:04 +0900

    ML_imple_LinearRegression_1

---
+++ Linear Regression/LinearRegression.py
@@ -0,0 +1,22 @@
+
+from re import X
+
+
+class LinearRegression:
+   def __init__(self, X,y):
+      self.X_train = X
+      self.y_train = y
+   
+   def fit(self,X, y):
+      ## Optimize the function
+      
+      return 
+   
+   
+   def predict(self, X):
+      # fit 에서 구한 함수에 그대로 대입한다.
+
+
+
+
+      return
